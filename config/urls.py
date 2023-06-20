3ecbe715a8d3cd96f4eacef61e2a75f3c4cc26fe
HNJNgithub <134996983+HNJNgithub@users.noreply.github.com>
2023-06-20 18:55:59 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,8 @@ from pybo.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pybo/', include('pybo.urls')),
+    path('', include('pybo.urls’)),
+#    path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
     path('', base_views.index, name='index'),  # '/' 에 해당되는 path
 ]

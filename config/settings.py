84ffcab8312de172d2a2eab8cc156268454affdd
HNJNgithub <134996983+HNJNgithub@users.noreply.github.com>
2023-06-20 18:54:57 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g*7p=#l5r^o$symhonf(jy+0bxkbgje)&^sbzkgj3605@45yu^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
